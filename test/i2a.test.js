89155d846e88653464d8d8e60eac30ab96834e56
Pieter Sheth-Voss <pieter@protobi.com>
2015-07-28 17:34:12 -0400

    Generate JSZip to drop dependency on file system; add additional unit tests.

--- test/i2a.test.js
+++ test/i2a.test.js
@@ -1,3 +1,5 @@
+var assert = require('assert');
+
 tool = {
   i2a: function(i) {
     var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
@@ -19,12 +21,18 @@ tool = {
     return getCellAdr(i);
   }
 }
-console.log( tool.i2a(25) == "Y", tool.i2a(25) +" not equial to 'Y'"+ "  "+ 25);
-console.log( tool.i2a(26) == "Z", tool.i2a(26) +" not equial to 'Z'"+ "  "+ 26);
-console.log( tool.i2a(27) == "AA", tool.i2a(27) +" not equial to 'AA'"+ "  "+ 27);
-console.log( tool.i2a(52) == "AZ", tool.i2a(52) +" not equial to 'AZ'"+ "  "+ 52);
-console.log( tool.i2a(53) == "BA", tool.i2a(53) +" not equial to 'BA'"+ "  "+ 53);
-console.log( tool.i2a(78) == "BZ", tool.i2a(78) +" not equial to 'BZ'"+ "  "+ 78);
-console.log( tool.i2a(79) == "CA", tool.i2a(79) +" not equial to 'CA'"+ "  "+ 79);
-console.log( tool.i2a(702) == "ZZ", tool.i2a(702) +" not equial to 'ZZ'"+ "  "+ 702);
-console.log( tool.i2a(703) == "AAA", tool.i2a(703) +" not equial to 'AAA'"+ "  "+ 703);
+
+describe("i2a", function(){
+  it ("matches and non-matches appropriately", function() {
+    assert( tool.i2a(25) == "Y", tool.i2a(25) +" not equial to 'Y'"+ "  "+ 25);
+    assert( tool.i2a(26) == "Z", tool.i2a(26) +" not equial to 'Z'"+ "  "+ 26);
+    assert( tool.i2a(27) == "AA", tool.i2a(27) +" not equial to 'AA'"+ "  "+ 27);
+    assert( tool.i2a(52) == "AZ", tool.i2a(52) +" not equial to 'AZ'"+ "  "+ 52);
+    assert( tool.i2a(53) == "BA", tool.i2a(53) +" not equial to 'BA'"+ "  "+ 53);
+    assert( tool.i2a(78) == "BZ", tool.i2a(78) +" not equial to 'BZ'"+ "  "+ 78);
+    assert( tool.i2a(79) == "CA", tool.i2a(79) +" not equial to 'CA'"+ "  "+ 79);
+    assert( tool.i2a(702) == "ZZ", tool.i2a(702) +" not equial to 'ZZ'"+ "  "+ 702);
+    assert( tool.i2a(703) == "AAA", tool.i2a(703) +" not equial to 'AAA'"+ "  "+ 703);
+  });
+});
+
